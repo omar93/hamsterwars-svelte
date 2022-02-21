85e2f8397645a9567b864b19a69bf3b7f0c077c9
Omar Alhamad <omar93@hotmail.se>
2022-02-21 21:31:56 +0100

    initial

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const hamsters = require("./routes/hamsters.js");
+const path = require("path");
+const matches = require("./routes/matches");
+const matchWinners = require("./routes/matchWinners");
+const winners = require("./routes/winners");
+const losers = require("./routes/losers");
+
+const PORT = process.env.PORT || 3233;
+const staticFolder = path.join(__dirname, "../build");
+
+//Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method}  ${req.url} `, req.params);
+  next();
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(staticFolder));
+
+//Routes
+app.use("/hamsters", hamsters);
+app.use("/matches", matches);
+app.use("/matchWinners", matchWinners);
+app.use("/winners", winners);
+app.use("/losers", losers);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on http://localhost:" + PORT);
+});
